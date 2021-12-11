2a57c8811317aa27e99eecb9f396d26dd65382e7
tp02ga <trevor@craftycodr.com>
2021-12-11 10:12:20 -0500

    Setting up a new Spring Boot project

---
+++ src/main/java/com/coderscampus/AssignmentSubmissionApp/AssignmentSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.AssignmentSubmissionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentSubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/AssignmentSubmissionApp/AssignmentSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.AssignmentSubmissionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
