7ea349c0ca956c89be4f94c0a4272aa885af5d8a
tp02ga <trevor@craftycodr.com>
2021-12-13 09:22:48 -0500

    Added domain objects

---
+++ src/main/java/com/coderscampus/AssignmentSubmissionApp/domain/Assignment.java
@@ -0,0 +1,59 @@
+package com.coderscampus.AssignmentSubmissionApp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity // will create table called assignment (based on Class name) 
+public class Assignment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+    private String githubUrl;
+    private String branch;
+    private String codeReviewVideoUrl;
+    @ManyToOne(optional = false)
+    private User user;
+    // TODO: create private User assignedTo;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getGithubUrl() {
+        return githubUrl;
+    }
+    public void setGithubUrl(String githubUrl) {
+        this.githubUrl = githubUrl;
+    }
+    public String getBranch() {
+        return branch;
+    }
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+    public String getCodeReviewVideoUrl() {
+        return codeReviewVideoUrl;
+    }
+    public void setCodeReviewVideoUrl(String codeReviewVideoUrl) {
+        this.codeReviewVideoUrl = codeReviewVideoUrl;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+}
