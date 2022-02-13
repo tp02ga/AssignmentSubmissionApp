ec6adad9aa90b03d05c94ee31af681a310507cbe
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2022-02-12 18:48:48 -0600

    update broken link
    updates to homepage for link is broken

--- front-end/src/Homepage/index.js
+++ front-end/src/Homepage/index.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Login from "../Login"
 import logo from "./coders-campus-logo.png"
-import {Button, Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #243f93;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
 
 const Homepage = () => {
 	function goToRegister(){
@@ -16,8 +28,17 @@ const Homepage = () => {
       <Container>
         <Row>
           <Col md> <img src={logo} alt="logo" className="logo"/> </Col> 
-          <Col md><Button variant="primary" onClick={goToRegister}> Register </Button></Col>
-          <Col md><Button variant="primary" onClick={goToLogin}> Login </Button></Col>
+          <Col md>
+            <Router>
+              <nav> 
+                <Link to="/login">Login</Link> 
+              </nav>
+              <Routes>
+                <Route path="/login" element={<Login />}/> 
+              </Routes>
+            </Router>
+          </Col>
+          <Col md><Button variant="success" onClick={goToRegister}> Register </Button></Col>
         </Row>
       </Container>
     </div>
