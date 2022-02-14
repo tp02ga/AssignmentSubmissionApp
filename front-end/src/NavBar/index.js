e9308e4b3d2771b658356aa616843e2448cfbb2c
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2022-02-14 16:52:03 -0600

    changes to button in navbar
    changes to button in navbar

--- front-end/src/NavBar/index.js
+++ front-end/src/NavBar/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom';
 import styled from "styled-components";
+import {Button} from "react-bootstrap";
 
-const Button = styled.button`
-  background-color: #243f93;
-  color: white;
-  font-size: 15px;
-  padding: 10px 30px;
-  border-radius: 5px;
- 
- 
-`;
 function goToRegister(){
     window.location = "/register";
 }
@@ -18,7 +10,7 @@ function NavBar(){
     return(
         <div className="NavBar"> 
             <NavLink className="navLink" to="/login" >Login</NavLink>
-            <Button onClick={goToRegister}>Register</Button>
+            <Button variant="outline-primary" onClick={goToRegister}>Register</Button>
         </div>
     )
 }
