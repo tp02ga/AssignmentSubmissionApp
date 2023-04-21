e5730b06be99f2b0da1cdb4b90b649ea43abd37b
MagicBaconDonuts <ricardoalatorre89@gmail.com>
2023-04-20 18:30:01 -0700

    Styling the home view page

--- front-end/src/Homepage/index.js
+++ front-end/src/Homepage/index.js
@@ -2,13 +2,29 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import NavBar from "../NavBar";
 
+import './homepage.css';
+
 const Homepage = () => {
+  
   return (
     <>
       <NavBar />
-      <Container className="mt-5">
-        <h1>Welcome Fellow Coders</h1>
-      </Container>
+      <div className="wrapper">
+        <Container >
+          <div className="top-half">
+            <h1 className="welcome">Welcome Fellow Coders</h1>
+            <h2 className="welcome-description">
+              Coders Campus Assignment Submission Form is a tool to make turning in assignments more convenient. 
+              Students can insert Github links of their code for each individual assignment so a reviewer can clone code.
+            </h2>
+            </div>
+          </Container>
+          <Container>
+            <div className="bottom-half">
+              <h2 className="welcome-text">With these tools students can get personalized video feedback from coding experts. </h2>
+            </div>
+          </Container>
+      </div>
     </>
   );
 };
